ff29ef52bb8027f6f15fbffcd24efa230d4ce3e9
ShivanshBhatnagar8 <shivanshbhatnagar8@gmail.com>
2024-08-08 00:43:24 +0530

    Contains logic for Home page on the website

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,35 @@
+import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
+function Home() {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.home}>
+          <div className={styles.homeText}>
+            <h1>Find Your Favorites, Shop with Ease</h1>
+            <span className={styles.heroText}>
+              Browse our curated collection of top products, enjoy effortless
+              shopping, and benefit from speedy delivery. We make shopping
+              simple and enjoyable.
+            </span>
+            <div className={styles.buttons}>
+              <Link className={styles.getStartedBtn} to="products">
+                Get Started
+              </Link>
+              <Link className={styles.cartBtn} to="cart">
+                Go to Cart
+              </Link>
+            </div>
+          </div>
+          <img
+            className={styles.heroImg}
+            src="./src/assets/img/hero.jpg"
+            alt="Image of a lady with clothes"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
